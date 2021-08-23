5655ac2a0018325651cf07d80bcf5e38f2b9b707
谢毅 <2387911242@qq.com>
2021-08-23 09:52:39 +0800

    First submit

---
+++ .config.example.js
@@ -0,0 +1,16 @@
+export default  {
+  "accessId": "<!--Your access ID--->",
+  "accessSecret": "<!--Your access Secret--->",
+  "endpoint":"https://kms.cn-hangzhou.aliyuncs.com",
+  "kmsKeyId":"<!--Your kms Key ID--->",
+  "kmsKeyVersionId":"<!--Your kms version ID--->",
+  "publicKey":"-----BEGIN PUBLIC KEY-----\n" +
+      "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA3R6yHS/8KAzYuta+/3xP\n" +
+      "kl0hcSUEU8uJcYrQoDWnUBCdNVnSvjlvokJ/t3I8TSzrma0wSHxnR5fBxsF/d6HG\n" +
+      "JasyBG7/z2aNNv0PwvtcGJYOF3JhAVkpUKh/NJU8j5GSmt553gdgqyFZGVXCvUv0\n" +
+      "l3EkdrNk/xLWMw/G3p5PNKu1x+i+9anNxZrtvxqZshNRte/HSM0VeKMdh5NwJex6\n" +
+      "2g1n57Fwh4Ptn2oFNon/Kwttxo4Ozw6KHyBICSCaVK+ZFyQsCsGdDUlBtv4ub1fz\n" +
+      "bg4ThpysLh/8m/onSeHFP3TJaliopD6hqbLFEn8Q9QY/7TaEx0PQW7O+9pXIZS2W\n" +
+      "ewIDAQAB\n" +
+      "-----END PUBLIC KEY-----\n"
+}
